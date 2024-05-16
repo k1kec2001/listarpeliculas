b678c0208c8716bcf7bb97e4f12683223144d511
Enrique <enrique-2001@live.com>
2024-05-16 13:36:19 -0500

    CRUD-ADMIN

--- src/app/componentes/peliculas/PeliAdmin.tsx
+++ src/app/componentes/peliculas/PeliAdmin.tsx
@@ -1,4 +1,22 @@
+import { useState } from "react";
+import { Pelicula } from "../../modelos/Pelicula";
+import { ARREGLO_PELICULAS } from "../../mocks/Pelicula-mocks";
+import { ARREGLO_PELICULA_GENERO } from "../../utilidades/dominios/DomGenero";
+
+
+
 export const PeliAdmin = () => {
+  const [arrPeliculas] = useState<Pelicula[]>(ARREGLO_PELICULAS);
+  
+  const getNombreGenero = (valor: string) => {
+    for (const objGen of ARREGLO_PELICULA_GENERO) {
+      if (objGen.codGenero == valor) {
+        return objGen.nombreGenero;
+      }
+    }
+  };
+
+
     return (
       <>
         <div className="d-flex justify-content-center">
@@ -6,126 +24,39 @@ export const PeliAdmin = () => {
             <table className="table table-striped table-hover">
               <thead>
                 <tr className="table-danger">
-                  <th style={{ width: "10%" }}>Nro</th>
-                  <th style={{ width: "30%" }}>Nombre</th>
-                  <th style={{ width: "25%" }}>Protagonista</th>
-                  <th style={{ width: "25%" }}>Imagen</th>
+                  <th style={{ width: "10%" }}>Código</th>
+                  <th style={{ width: "25%" }}>Nombre</th>
+                  <th style={{ width: "15%" }}>Género</th>
+                  <th style={{ width: "20%" }}>Protagonista</th>
+                  <th style={{ width: "20%" }}>Imagen</th>
                   <th style={{ width: "10%" }}> </th>
                 </tr>
               </thead>
               <tbody>
-                <tr className="align-middle">
-                  <td>1</td>
-                  <td>Top Gun</td>
-                  <td>Tom Cruise</td>
-                  <td>fotico.png</td>
-                  <td className="text-center">
-                    <a href="/#">
-                      <i className="fa-solid fa-trash-can rojito"></i>
-                    </a>{" "}
-                    <a href="/pactual/1">
-                      <i className="fa-regular fa-pen-to-square verde"></i>
-                    </a>
-                  </td>
-                </tr>
-                <tr className="align-middle">
-                  <td>2</td>
-                  <td>Jason vs Freddy</td>
-                  <td>Jason krugeur</td>
-                  <td></td>
-                  <td className="text-center">
-                    <a href="/#">
-                      <i className="fa-solid fa-trash-can rojito"></i>
-                    </a>{" "}
-                    <a href="/pactual/2">
-                      <i className="fa-regular fa-pen-to-square verde"></i>
-                    </a>
-                  </td>
-                </tr>
-                <tr className="align-middle">
-                  <td>3</td>
-                  <td>Dragon Ball GT</td>
-                  <td>Goku</td>
-                  <td></td>
-                  <td className="text-center">
-                    <a href="/#">
-                      <i className="fa-solid fa-trash-can rojito"></i>
-                    </a>{" "}
-                    <a href="/pactual/3">
-                      <i className="fa-regular fa-pen-to-square verde"></i>
-                    </a>
-                  </td>
-                </tr>
-                <tr className="align-middle">
-                  <td>4</td>
-                  <td>Matanza en Texas</td>
-                  <td>KIll perro</td>
-                  <td>matanza.jpg</td>
-                  <td className="text-center">
-                    <a href="/#">
-                      <i className="fa-solid fa-trash-can rojito"></i>
-                    </a>{" "}
-                    <a href="/pactual/4">
-                      <i className="fa-regular fa-pen-to-square verde"></i>
-                    </a>
-                  </td>
-                </tr>
-                <tr className="align-middle">
-                  <td>5</td>
-                  <td>Tiburón IV</td>
-                  <td>El shark</td>
-                  <td></td>
-                  <td className="text-center">
-                    <a href="/#">
-                      <i className="fa-solid fa-trash-can rojito"></i>
-                    </a>{" "}
-                    <a href="/pactual/5">
-                      <i className="fa-regular fa-pen-to-square verde"></i>
-                    </a>
-                  </td>
-                </tr>
-                <tr className="align-middle">
-                  <td>6</td>
-                  <td>Start trek</td>
-                  <td>Capitan Kirk</td>
-                  <td></td>
-                  <td className="text-center">
-                    <a href="/#">
-                      <i className="fa-solid fa-trash-can rojito"></i>
-                    </a>{" "}
-                    <a href="/pactual/6">
-                      <i className="fa-regular fa-pen-to-square verde"></i>
-                    </a>
-                  </td>
-                </tr>
-                <tr className="align-middle">
-                  <td>7</td>
-                  <td>Dioses del olimpo</td>
-                  <td>Zeus dos</td>
-                  <td></td>
-                  <td className="text-center">
-                    <a href="/#">
-                      <i className="fa-solid fa-trash-can rojito"></i>
-                    </a>{" "}
-                    <a href="/pactual/7">
-                      <i className="fa-regular fa-pen-to-square verde"></i>
-                    </a>
-                  </td>
-                </tr>
-                <tr className="align-middle">
-                  <td>8</td>
-                  <td>Un pistolero rudo</td>
-                  <td>Balitas</td>
-                  <td></td>
-                  <td className="text-center">
-                    <a href="/#">
-                      <i className="fa-solid fa-trash-can rojito"></i>
-                    </a>{" "}
-                    <a href="/pactual/8">
-                      <i className="fa-regular fa-pen-to-square verde"></i>
-                    </a>
-                  </td>
-                </tr>
+                {arrPeliculas.map((miPeli:Pelicula)=>(
+                <tr className="align-middle" key={miPeli.codPelicula}>
+                <td>{miPeli.codPelicula}</td>
+                <td>{miPeli.nombrePelicula}</td>
+                <td>{getNombreGenero(miPeli.codGeneroPelicula)}</td>
+                <td>{miPeli.protagonistaPelicula}</td>
+                <td>                 
+                   <img src={miPeli.imagenPeliculaBase64} alt="LogoPelicula" className="imagenListado" />
+                    <div className="text-info">
+                      {miPeli.imagenPelicula}
+                    </div></td>
+                <td className="text-center">
+                  <a href="/#">
+                    <i className="fa-solid fa-trash-can rojito"></i>
+                  </a>{" "}
+                  <a href="/pactual/:codigo">
+                    <i className="fa-regular fa-pen-to-square verde"></i>
+                  </a>
+                </td>
+              </tr>
+
+                ))}
+
+
               </tbody>
             </table>
           </div>
